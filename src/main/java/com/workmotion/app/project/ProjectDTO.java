79d1ce66368543484c67b132b44304a743cd8f95
devkgm <gm.kim333@gmail.com>
2024-01-31 12:40:37 +0900

    project shema

---
+++ src/main/java/com/workmotion/app/project/ProjectDTO.java
@@ -0,0 +1,10 @@
+package com.workmotion.app.project;
+
+import java.util.Date;
+
+public class ProjectDTO {
+    private Long id;
+    private String name;
+    private Date create_dt;
+    private String info;
+}
