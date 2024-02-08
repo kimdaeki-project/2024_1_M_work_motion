e8a4cbe00191ba6767f647a6cd47d7e0924785a8
parKsungjea <sungjea15@naver.com>
2024-02-08 15:19:05 +0900

    디벨롭 풀 하기위한 커밋..

--- src/main/java/com/workmotion/app/board/BoardDTO.java
+++ src/main/java/com/workmotion/app/board/BoardDTO.java
@@ -1,5 +1,15 @@
 package com.workmotion.app.board;
 
+import java.sql.Date;
+
 public class BoardDTO {
 
+	private Long id;
+	private String title;
+	private String content;
+	private Long views;
+	private Date create_dt;
+	private Long category_id;
+	
+	
 }
