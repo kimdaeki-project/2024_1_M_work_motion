4cea8e2f87491bfdff070b60e24e071cc66e4cf1
devkgm <gm.kim333@gmail.com>
2024-02-20 15:19:03 +0900

    feat on join set default avatar

--- src/main/java/com/workmotion/app/member/MemberController.java
+++ src/main/java/com/workmotion/app/member/MemberController.java
@@ -1,10 +1,6 @@
 package com.workmotion.app.member;
 
 
-import java.util.StringTokenizer;
-
-import javax.servlet.http.HttpSession;
-
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,123 +11,126 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
+import java.util.StringTokenizer;
+
 
 @Controller
 @RequestMapping(value = "/member/*")
 public class MemberController {
-	
-	@Autowired
-	private MemberService memberService;
-	
-	
-	
-	@ResponseBody
-	@PostMapping("pwCheck")
-	public int pwCheck(HttpSession session,String pass,MemberDTO memberDTO) throws Exception {
-		MemberDTO m = (MemberDTO)session.getAttribute("member");
-		 	m = memberService.getlogin(m);
-		 	StringTokenizer tokenizer = new StringTokenizer(pass,",");
-		 		memberDTO.setPassword(tokenizer.nextToken());	
-		 	int result=0;
-			if(BCrypt.checkpw(memberDTO.getPassword(),m.getPassword())) {
-				result=1;
-			}
-			return result;
-	}
-	
-	@ResponseBody
-	@GetMapping("emailcheck")
-	public int getemailcheck(MemberDTO memberDTO,Model model) throws Exception {
-		int result = 0;	
-		memberDTO = memberService.emailCheck(memberDTO);
-		if(memberDTO==null){
-			result = 1;
-		}
-		return result;
-	}
-	
-	
-	@GetMapping("agree")
-	public void getagree()throws Exception {
-	}
-	
-	
-	@GetMapping("logout")
-	public String getlogout(HttpSession session)throws Exception {
-		session.invalidate();
-		return "/member/login";
-	}
-	
-  
-	@GetMapping("login")
-	public void getlogin()throws Exception{	
-	}
-	@PostMapping("login")
-	public String getlogin(MemberDTO memberDTO,HttpSession session,Model model)throws Exception{
-		MemberDTO m = memberService.getlogin(memberDTO);
-			if(m !=null) {
-				if(BCrypt.checkpw(memberDTO.getPassword(),m.getPassword())) {
-					m.setPassword(null);
-					session.setAttribute("member",m);
-					model.addAttribute("page","home");
-					return "index";	
-				}else {
-					//비번이 다른경우
-					model.addAttribute("msg","비밀번호를 확인해주세요");
-					return"member/login";
-				}
-			}else {
-				//아이디가 다른경우
-				model.addAttribute("msg","아이디를 확인해주세요");
-				return"member/login";
-			}
 
-	}
-	
-	@PostMapping("join")
-	public String getjoin(MemberDTO memberDTO,Model model)throws Exception{
-		String hashpassword = BCrypt.hashpw(memberDTO.getPassword(),BCrypt.gensalt());
-		memberDTO.setPassword(hashpassword);
-		int result =  memberService.getjoin(memberDTO);
-		return "member/login";
-	}
-	
-	@GetMapping("join")
-	public String getjoin(Model model)throws Exception{
-		model.addAttribute("page","member/join");
-		return "member/join";
-	}
-	
-	
-	
-	
-	@GetMapping("mypage")
-	public String mypage(HttpSession session,Model model)throws Exception {
-		MemberDTO m = (MemberDTO)session.getAttribute("member");
-		if(m==null) {
-			model.addAttribute("page","home");
-		}else {
-			MemberDTO memberDTO = memberService.detailMember(m);
-			model.addAttribute("dto",memberDTO);
-			model.addAttribute("page","member/mypage");
-		}
-		return "index";
-	}
-	
-	
-	@PostMapping("update")
-	public String getupdate(MemberDTO memberDTO,Model model,MultipartFile picture) throws Exception { 
-		if(picture!=null) {
-			memberService.setFileDelete(memberDTO);
-			memberService.setFileAdd(memberDTO,picture);			
-		}
-		if(memberDTO.getPassword()!=null) {
-			String hashpassword = BCrypt.hashpw(memberDTO.getPassword(),BCrypt.gensalt());
-			memberDTO.setPassword(hashpassword);
-		}
-		int result = memberService.updateMember(memberDTO);
-		 model.addAttribute("page","home");
-		 return "index";
-	}
-	
+    @Autowired
+    private MemberService memberService;
+
+
+    @ResponseBody
+    @PostMapping("pwCheck")
+    public int pwCheck(HttpSession session, String pass, MemberDTO memberDTO) throws Exception {
+        MemberDTO m = (MemberDTO) session.getAttribute("member");
+        m = memberService.getlogin(m);
+        StringTokenizer tokenizer = new StringTokenizer(pass, ",");
+        memberDTO.setPassword(tokenizer.nextToken());
+        int result = 0;
+        if (BCrypt.checkpw(memberDTO.getPassword(), m.getPassword())) {
+            result = 1;
+        }
+        return result;
+    }
+
+    @ResponseBody
+    @GetMapping("emailcheck")
+    public int getemailcheck(MemberDTO memberDTO, Model model) throws Exception {
+        int result = 0;
+        memberDTO = memberService.emailCheck(memberDTO);
+        if (memberDTO == null) {
+            result = 1;
+        }
+        return result;
+    }
+
+
+    @GetMapping("agree")
+    public void getagree() throws Exception {
+    }
+
+
+    @GetMapping("logout")
+    public String getlogout(HttpSession session) throws Exception {
+        session.invalidate();
+        return "/member/login";
+    }
+
+
+    @GetMapping("login")
+    public void getlogin() throws Exception {
+    }
+
+    @PostMapping("login")
+    public String getlogin(MemberDTO memberDTO, HttpSession session, Model model) throws Exception {
+        MemberDTO m = memberService.getlogin(memberDTO);
+        if (m != null) {
+            if (BCrypt.checkpw(memberDTO.getPassword(), m.getPassword())) {
+                m.setPassword(null);
+                session.setAttribute("member", m);
+                model.addAttribute("page", "home");
+                return "index";
+            } else {
+                //비번이 다른경우
+                model.addAttribute("msg", "비밀번호를 확인해주세요");
+                return "member/login";
+            }
+        } else {
+            //아이디가 다른경우
+            model.addAttribute("msg", "아이디를 확인해주세요");
+            return "member/login";
+        }
+
+    }
+
+    @PostMapping("join")
+    public String getjoin(MemberDTO memberDTO, Model model) throws Exception {
+        String hashpassword = BCrypt.hashpw(memberDTO.getPassword(), BCrypt.gensalt());
+        memberDTO.setPassword(hashpassword);
+        int result = memberService.getjoin(memberDTO);
+        memberService.setFileAdd(memberDTO);
+        System.out.println(memberDTO.getId());
+        return "member/login";
+    }
+
+    @GetMapping("join")
+    public String getjoin(Model model) throws Exception {
+        model.addAttribute("page", "member/join");
+        return "member/join";
+    }
+
+
+    @GetMapping("mypage")
+    public String mypage(HttpSession session, Model model) throws Exception {
+        MemberDTO m = (MemberDTO) session.getAttribute("member");
+        if (m == null) {
+            model.addAttribute("page", "home");
+        } else {
+            MemberDTO memberDTO = memberService.detailMember(m);
+            model.addAttribute("dto", memberDTO);
+            model.addAttribute("page", "member/mypage");
+        }
+        return "index";
+    }
+
+
+    @PostMapping("update")
+    public String getupdate(MemberDTO memberDTO, Model model, MultipartFile picture) throws Exception {
+        if (picture != null) {
+            memberService.setFileDelete(memberDTO);
+            memberService.setFileAdd(memberDTO, picture);
+        }
+        if (memberDTO.getPassword() != null) {
+            String hashpassword = BCrypt.hashpw(memberDTO.getPassword(), BCrypt.gensalt());
+            memberDTO.setPassword(hashpassword);
+        }
+        int result = memberService.updateMember(memberDTO);
+        model.addAttribute("page", "home");
+        return "index";
+    }
+
 }
--- src/main/java/com/workmotion/app/member/MemberService.java
+++ src/main/java/com/workmotion/app/member/MemberService.java
@@ -1,72 +1,72 @@
 package com.workmotion.app.member;
 
 
-
-
-
-
-
-import javax.servlet.ServletContext;
-
+import com.workmotion.app.util.FileManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.workmotion.app.util.FileManager;
-
+import javax.servlet.ServletContext;
 
 
 @Service
 public class MemberService {
 
-	@Autowired
-	private MemberDAO memberDAO;
-	@Autowired
-	private FileManager fileManager;
-	@Autowired
-	private ServletContext servletContext;
+    @Autowired
+    private MemberDAO memberDAO;
+    @Autowired
+    private FileManager fileManager;
+    @Autowired
+    private ServletContext servletContext;
 
-	
-	public MemberDTO getlogin(MemberDTO memberDTO) throws Exception {
-		MemberDTO m = memberDAO.detailMember(memberDTO);
-			return m;
-		
-	}
-	
-	public void setFileAdd (MemberDTO memberDTO,MultipartFile picture) throws Exception {
-		String path = servletContext.getRealPath("resources/upload/member");
-		String fileName = fileManager.fileSave(path, picture);
-		Avatar avatar = new Avatar();
-		if(picture.isEmpty()) {
-			return;
-		}else {
-			avatar.setMember_id(memberDTO.getId());
-			avatar.setName(fileName);
-			avatar.setOri_name(picture.getOriginalFilename());	
-			memberDAO.setFileAdd(avatar);			
-		}
-	}
-	public void setFileDelete (MemberDTO memberDTO) throws Exception {
-		MemberDTO m = memberDAO.detailMember(memberDTO);
-		String path = servletContext.getRealPath("resources/upload/member");
-			if(m.getAvatar().getName()!=null) {
-			fileManager.fileDelete(path, m.getAvatar().getName());			
-			memberDAO.setFileDelete(m.getAvatar());
-		}
 
-	}
-	 
-	public int updateMember(MemberDTO memberDTO) throws Exception {
-		return	memberDAO.updateMember(memberDTO);
-	}
-	public MemberDTO detailMember (MemberDTO memberDTO) throws Exception {
-		return memberDAO.detailMember(memberDTO);
-	}
-	public MemberDTO emailCheck(MemberDTO memberDTO) throws Exception {
-		return memberDAO.emailCheck(memberDTO);
-	}
+    public MemberDTO getlogin(MemberDTO memberDTO) throws Exception {
+        MemberDTO m = memberDAO.detailMember(memberDTO);
+        return m;
 
-	public int getjoin(MemberDTO memberDTO) throws Exception {
-		return memberDAO.createMember(memberDTO);
-	}
+    }
+
+    public void setFileAdd(MemberDTO memberDTO, MultipartFile picture) throws Exception {
+        String path = servletContext.getRealPath("resources/upload/member");
+        String fileName = fileManager.fileSave(path, picture);
+        Avatar avatar = new Avatar();
+        if (picture.isEmpty()) {
+            return;
+        } else {
+            avatar.setMember_id(memberDTO.getId());
+            avatar.setName(fileName);
+            avatar.setOri_name(picture.getOriginalFilename());
+            memberDAO.setFileAdd(avatar);
+        }
+    }
+
+    public int setFileAdd(MemberDTO memberDTO) throws Exception {
+        return memberDAO.setFileAdd(memberDTO);
+    }
+
+    public void setFileDelete(MemberDTO memberDTO) throws Exception {
+        MemberDTO m = memberDAO.detailMember(memberDTO);
+        String path = servletContext.getRealPath("resources/upload/member");
+        if (m.getAvatar().getName() != null) {
+            fileManager.fileDelete(path, m.getAvatar().getName());
+            memberDAO.setFileDelete(m.getAvatar());
+        }
+
+    }
+
+    public int updateMember(MemberDTO memberDTO) throws Exception {
+        return memberDAO.updateMember(memberDTO);
+    }
+
+    public MemberDTO detailMember(MemberDTO memberDTO) throws Exception {
+        return memberDAO.detailMember(memberDTO);
+    }
+
+    public MemberDTO emailCheck(MemberDTO memberDTO) throws Exception {
+        return memberDAO.emailCheck(memberDTO);
+    }
+
+    public int getjoin(MemberDTO memberDTO) throws Exception {
+        return memberDAO.createMember(memberDTO);
+    }
 }
