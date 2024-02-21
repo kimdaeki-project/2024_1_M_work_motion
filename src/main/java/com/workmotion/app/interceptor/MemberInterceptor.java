073e705051de64f1a3c4a5065bd02967084848d8
dirokim <dirokim7@gmail.com>
2024-02-21 17:23:36 +0900

    fix interceptor

--- src/main/java/com/workmotion/app/interceptor/MemberInterceptor.java
+++ src/main/java/com/workmotion/app/interceptor/MemberInterceptor.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.workmotion.app.member.MemberDTO;
+
 @Component
 public class MemberInterceptor extends HandlerInterceptorAdapter{
 	
@@ -15,14 +17,13 @@ public class MemberInterceptor extends HandlerInterceptorAdapter{
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 			HttpSession session = request.getSession();
-			Object obj = session.getAttribute("member");
+			Object obj = (MemberDTO)session.getAttribute("member");
 			if(obj != null) {
 				return true; 
 			}else {
 				request.setAttribute("msg","로그인이 필요한 서비스 입니다");
-				request.setAttribute("page","commons/result");
 				request.setAttribute("path","/member/login");
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/commons/result.jsp");
 				rd.forward(request, response);
 				return false;
 			}
--- src/main/webapp/WEB-INF/views/member/login.jsp
+++ src/main/webapp/WEB-INF/views/member/login.jsp
@@ -66,7 +66,7 @@ pageEncoding="UTF-8"%>
      <!-- Bootstrap JS -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-     <script src="/resources/js/scripts.js"></script>
+     <script src="/resources/js/scriptsMember.js"></script>
 	 
     </body>
 </html>
