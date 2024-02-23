072490cc7daf2428b4d5b57cc3a9f079479f1d04
김경모 <gm.kim333@gmail.com>
2024-02-24 00:43:02 +0900

    feat functions

--- src/main/java/com/workmotion/app/notification/NotificationController.java
+++ src/main/java/com/workmotion/app/notification/NotificationController.java
@@ -1,10 +1,26 @@
 package com.workmotion.app.notification;
 
+import com.workmotion.app.member.MemberDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
+@RequestMapping("/v1/*")
 public class NotificationController {
     @Autowired
     private NotificationService notificationService;
+
+    @GetMapping("/notifications")
+    public ResponseEntity<List<NotificationDTO>> getNotifications(HttpSession session) throws Exception {
+        return ResponseEntity.ok().body(notificationService.getNotification((MemberDTO) session.getAttribute("member")));
+    }
+
+    @PutMapping("/notifications/{id}")
+    public ResponseEntity<?> updateNotification(NotificationDTO notificationDTO, HttpSession session, @PathVariable String id) throws Exception {
+        return ResponseEntity.ok().body(notificationService.updateNotification(notificationDTO, id));
+    }
 }
--- src/main/java/com/workmotion/app/notification/NotificationService.java
+++ src/main/java/com/workmotion/app/notification/NotificationService.java
@@ -1,10 +1,30 @@
 package com.workmotion.app.notification;
 
+import com.workmotion.app.member.MemberDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class NotificationService {
     @Autowired
     private NotificationDAO notificationDAO;
+
+    public List<NotificationDTO> getNotification(MemberDTO memberDTO) throws Exception {
+        return notificationDAO.getNotification(memberDTO);
+    }
+
+    public int addNotification(NotificationDTO notificationDTO) throws Exception {
+        return notificationDAO.addNotification(notificationDTO);
+    }
+
+    public int updateNotification(NotificationDTO notificationDTO, String id) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("notification", notificationDTO);
+        return notificationDAO.updateNotification(map);
+    }
 }
