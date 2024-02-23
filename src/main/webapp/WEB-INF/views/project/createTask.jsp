8bd742192b8ea1ab1c8891acd878a0723628116f
김경모 <gm.kim333@gmail.com>
2024-02-24 02:19:03 +0900

    style 멤버 리스트 통일화

--- src/main/webapp/WEB-INF/views/project/createTask.jsp
+++ src/main/webapp/WEB-INF/views/project/createTask.jsp
@@ -1,7 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 pageEncoding="UTF-8"%> <%@ taglib uri="http://java.sun.com/jsp/jstl/core"
 prefix="c" %>
-
+<link
+    rel="stylesheet"
+    href="/resources/css/project/createTask.css"
+    type="text/css"
+/>
 <div class="container mt-3">
     <form
         action="/tasks/create"
--- src/main/webapp/resources/js/project/edit.js
+++ src/main/webapp/resources/js/project/edit.js
@@ -27,17 +27,32 @@ function createCrewList(memberList) {
     let html = "";
     for (member of memberList) {
         html += `
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="member_id" value="${member.id}">
-                    <div class="memberCard">
-                        <div class="avatar">
-                        </div>
-                        <div class="info">
-                            <div class="name">${member.name}</div>
-                            <div class="role">${member.position.name}</div>
-                        </div>
+        <div class="d-flex form-check align-items-center">
+            <input class="form-check-input me-3" type="checkbox" name="member_id" value="${member.id}" id="checbox${member.id}">
+            <label
+                    class="d-flex align-items-center pb-1 w-100"
+                    id="tooltips-container"
+                    for="checbox${member.id}"
+                >
+                    <img
+                        src="${member.avatar.name}"
+                        class="rounded-circle img-fluid avatar-md img-thumbnail bg-transparent"
+                        alt=""
+                    />
+                    <div class="w-100 ms-2">
+                        <h5 class="mb-1">
+                            ${member.name}<i
+                                class="mdi mdi-check-decagram text-info ms-1"
+                            ></i>
+                        </h5>
+                        <p
+                            class="mb-0 font-13 text-muted"
+                        >
+                            ${member.position.name}
+                        </p>
                     </div>
-                </div>
+                </label>
+        </div>
             `;
     }
     return html;
