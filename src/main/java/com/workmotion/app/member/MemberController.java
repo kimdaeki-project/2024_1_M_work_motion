13ae19b5329e920403e0102ad82673fe028bdd63
dirokim <dirokim7@gmail.com>
2024-02-26 17:56:03 +0900

    feat find password

--- src/main/java/com/workmotion/app/member/MemberController.java
+++ src/main/java/com/workmotion/app/member/MemberController.java
@@ -4,13 +4,18 @@ package com.workmotion.app.member;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Random;
 import java.util.StringTokenizer;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,15 +40,58 @@ public class MemberController {
 	private MemberService memberService;
 	@Autowired
 	private TossPaymentService tossPaymentService;
-
+	@Autowired
+	private JavaMailSender mailSenderNaver;
+	
+	
+	
+	@PostMapping("findPassWord")
+	public int findPassWord(MemberDTO memberDTO,Model model) throws Exception {
+		int result = 0;
+		if(memberDTO != null) {
+			MemberDTO m = memberService.detailMember(memberDTO);			
+			if(m !=null) {   // 존재하는 아이디면
+				Random r = new Random();
+				int num = r.nextInt(999999);  //랜덤 난수 6자리
+				StringBuilder sb = new StringBuilder();
+				String email = m.getEmail();
+				String social = email.substring(email.indexOf('@')+1, email.indexOf('.'));
+				String setFrom = "devksk7@naver.com";//발신자 이메일
+				String tomail = memberDTO.getEmail();
+				String title = "WorkMotion 비밀번호 변경 인증 이메일입니다";
+				sb.append(String.format("안녕하세요 %s님\n",memberDTO.getName()));
+				sb.append(String.format("WorkMotion 비밀번호: 임시 비밀 번호는  %d입니다.", num));
+				String content = sb.toString();
+				MimeMessage msg = mailSenderNaver.createMimeMessage();
+				MimeMessageHelper msgHelper = new MimeMessageHelper(msg, true, "utf-8");
+				msgHelper.setFrom(setFrom);
+				msgHelper.setTo(tomail);
+				msgHelper.setSubject(title);
+				msgHelper.setText(content);
+				// 메일 전송
+				mailSenderNaver.send(msg);
+				result = 2;
+				String sum = ""+num;
+				String hashpassword = BCrypt.hashpw(sum, BCrypt.gensalt());
+				m.setPassword(hashpassword);
+				memberService.updateMember(m);
+				return result;
+			}else {//존재하지 않는 이메일인 경우 -> 가입되지 않은 사용자 -> 회원가입 할래?
+				return result = 1;
+				}		
+		}else {		// 파라미터가 들어오지 않을 경우
+				result = 0;
+			return result;
+		}
+		}
+		
+		
 	
 	
 	@PostMapping("findId")
 	@ResponseBody
 	public String findId(MemberDTO memberDTO) throws Exception {
 		String msg ="" ;
-		System.out.println(memberDTO.getName());
-		System.out.println(memberDTO.getPhone());
 		if(memberDTO!=null) {
 			memberDTO = memberService.findId(memberDTO);
 			if(memberDTO !=null) {
@@ -103,27 +151,15 @@ public class MemberController {
 				TossPaymentDTO tossPaymentDTO = new TossPaymentDTO();
 				tossPaymentDTO.setCompany_id(m.getCompany_id());
 				tossPaymentDTO = tossPaymentService.getTossPaymentDetail(tossPaymentDTO);
-				System.out.println(tossPaymentDTO);
 				if(tossPaymentDTO != null) { 
 				String tossdate = tossPaymentDTO.getCreate_dt();		
-				System.out.println(tossPaymentDTO);
 				String [] ar = tossdate.split(" ");
 				String [] setDate = ar[0].split("-");
 				String [] setDate1 = ar[1].split(":");
-				System.out.println(setDate[0]);
-				System.out.println(setDate[1]);
-				System.out.println(setDate[2]);
-				System.out.println(setDate1[0]);
-				System.out.println(setDate1[1]);
-				System.out.println(setDate1[2]);
 				LocalDateTime startDt = LocalDateTime.now();
 				LocalDateTime lastDt = LocalDateTime.of(Integer.parseInt(setDate[0]), Integer.parseInt(setDate[1]), Integer.parseInt(setDate[2]), Integer.parseInt(setDate1[0]), Integer.parseInt(setDate1[1]));
-				System.out.println("startDt : "+startDt);
-				System.out.println(tossPaymentDTO.getPeriod());
 				lastDt = lastDt.plusDays(tossPaymentDTO.getPeriod());
-				System.out.println("lastDt : "+lastDt);
 				Long getdate = ChronoUnit.MILLIS.between(startDt, lastDt);
-				System.out.println("getdate : "+getdate);
 				session.setAttribute("toss", tossPaymentDTO.getPeriod());
 				if (tossPaymentDTO != null || getdate > 0) {
 					session.setAttribute("check", "yes");
