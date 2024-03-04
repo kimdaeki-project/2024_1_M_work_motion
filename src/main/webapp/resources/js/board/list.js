4ae9b7222198d934abfd40f9fff8a398a97bb27b
김경모 <gm.kim333@gmail.com>
2024-03-04 10:15:21 +0900

    fix 링크이동 스크립트

--- src/main/webapp/resources/js/board/list.js
+++ src/main/webapp/resources/js/board/list.js
@@ -3,11 +3,7 @@ const listTitle = document.querySelectorAll(".listTitle");
 
 for (let i = 0; i < listTitle.length; i++) {
     listTitle[i].addEventListener("click", function (e) {
-        let href;
-        let child = e.target.childNodes;
-        for (let j = 0; j < child.length; j++) {
-            href = child[j].href;
-        }
-        window.location.href = href;
+        let child = e.target.children;
+        window.location.href = child[0].href;
     });
 }
